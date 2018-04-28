219da22664d1c173636d1e40cf50e6b128e99a76
DrewVanD <37640375+DrewVanD@users.noreply.github.com>
2018-04-28 13:40:39 -0400

    Title Screen Complete

--- app/src/main/java/com/drew/teamawesomegame/MainActivity.java
+++ app/src/main/java/com/drew/teamawesomegame/MainActivity.java
@@ -1,17 +1,27 @@
 package com.drew.teamawesomegame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TEST TEST TESTicle
-        //matt matt test 2
-        //Chris test
-        
+
+        Button button = findViewById(R.id.playButton);
+        button.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, mainMenu.class);
+        startActivity(i);
     }
 }
