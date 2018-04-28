733e180261aea7ca9e66a71185d4fe873e316c53
DrewVanD <37640375+DrewVanD@users.noreply.github.com>
2018-04-28 13:57:15 -0400

    Starting Main Menu
    Got all the buttons on event listeners just need to figure out how to work the switch in the onClick

--- app/src/main/java/com/drew/teamawesomegame/mainMenu.java
+++ app/src/main/java/com/drew/teamawesomegame/mainMenu.java
@@ -2,12 +2,32 @@ package com.drew.teamawesomegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class mainMenu extends AppCompatActivity {
+public class mainMenu extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(this);
+
+        Button statsButton = findViewById(R.id.statsButton);
+        statsButton.setOnClickListener(this);
+
+        Button storeButton = findViewById(R.id.storeButton);
+        storeButton.setOnClickListener(this);
+
+        Button aboutButton = findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
